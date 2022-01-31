7fa187943c52bf0896adecf9e50ca9a9e1ca5ef6
sujan <sujankapali50@gmail.com>
2022-01-31 12:50:13 +0545

    added in navigation

--- js/front.js
+++ js/front.js
@@ -1,6 +1,11 @@
 const login = document.getElementById("login");
+const register = document.getElementById("register");
 const hide = document.getElementById("hide");
+const hideRegister = document.getElementById("hide-register");
 
 login.addEventListener("click", () => {
   hide.classList.toggle("hide");
+});
+register.addEventListener("click", () => {
+  hideRegister.classList.toggle("hide-register");
 });
